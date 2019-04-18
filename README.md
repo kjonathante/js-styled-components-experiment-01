87cc0406c18d96179ef9a515d4094f9d73b79d28
Kit Jonathan Te <kjonathante@gmail.com>
2019-04-18 10:24:34 -0700

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# js-styled-components-experiment-01
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import styled from "styled-components";
+
+// import "./styles.css";
+
+// Create a Title component that'll render an <h1> tag with some styles
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+// Create a Wrapper component that'll render a <section> tag with some styles
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
+
+function App() {
+  return (
+    <Wrapper>
+      <Title>Hello World!</Title>
+    </Wrapper>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
