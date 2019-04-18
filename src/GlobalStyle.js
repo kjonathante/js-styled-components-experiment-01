2305ba329ef3b853597e911d32cc6503290a2093
Kit Jonathan Te <kjonathante@gmail.com>
2019-04-18 11:23:13 -0700

    Refactored code and created a component folder.

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,8 +1,9 @@
-import { createGlobalStyle } from 'styled-components'
+import React from "react";
+import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
   body {
     @import url('https://fonts.googleapis.com/css?family=Notable');
     font-family: 'Notable', sans-serif;
   }
-`
+`;
--- src/index.js
+++ src/index.js
@@ -1,28 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import styled from "styled-components";
-import {GlobalStyle} from "./GlobalStyle";
 
-// Create a Title component that'll render an <h1> tag with some styles
-const Title = styled.h1`
-  font-size: 1.5em;
-  text-align: center;
-  color: palevioletred;
-`;
-
-// Create a Wrapper component that'll render a <section> tag with some styles
-const Wrapper = styled.section`
-  padding: 4em;
-  background: papayawhip;
-`;
+import { GlobalStyle } from "./GlobalStyle";
+import { HelloWorld } from "./components/HelloWorld";
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <Wrapper>
-        <Title>Hello World!</Title>
-      </Wrapper>
+      <HelloWorld />
     </>
   );
 }
